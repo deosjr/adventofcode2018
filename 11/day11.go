9c4f6d91b88e060733583bedbfe51ecc151d2734
Sjoerd Dost <deosjr@gmail.com>
2018-12-11 17:40:30 +0100

    fix concurrency bug, add preprocess step

--- 11/day11.go
+++ 11/day11.go
@@ -5,13 +5,15 @@ import (
 	"math"
 )
 
+const maxSize = 300
+
 type coord struct {
 	x, y int
 }
 
-func powerLevel(c coord, serial int) int {
-	rackID := c.x + 10
-	p := rackID * c.y
+func powerLevel(x, y, serial int) int {
+	rackID := x + 10
+	p := rackID * y
 	p += serial
 	p *= rackID
 	p = (p / 100) % 10
@@ -19,23 +21,36 @@ func powerLevel(c coord, serial int) int {
 	return p
 }
 
-func squareSum(minX, minY, size, serial int) int {
-	sum := 0
-	for y := minY; y < minY+size; y++ {
-		for x := minX; x < minX+size; x++ {
-			sum += powerLevel(coord{x, y}, serial)
+func maxPower(size, serial int) (coord, int) {
+	intermediate := map[coord]int{}
+	limit := maxSize - size + 1
+	for y := 1; y <= limit; y++ {
+		for x := 1; x <= maxSize; x++ {
+			if y == 1 {
+				sum := 0
+				for yy := 1; yy <= size; yy++ {
+					sum += powerLevel(x, yy, serial)
+				}
+				intermediate[coord{x, y}] = sum
+				continue
+			}
+			sum := intermediate[coord{x, y - 1}] - powerLevel(x, y-1, serial) + powerLevel(x, y+size-1, serial)
+			intermediate[coord{x, y}] = sum
 		}
 	}
-	return sum
-}
 
-func maxPower(size, serial int) (coord, int) {
 	var xans, yans int
 	max := math.MinInt64
-	limit := 300 - size + 2
-	for y := 1; y < limit; y++ {
-		for x := 1; x < limit; x++ {
-			sum := squareSum(x, y, size, serial)
+	for y := 1; y <= limit; y++ {
+		var sum int
+		for x := 1; x <= limit; x++ {
+			if x == 1 {
+				for xx := 1; xx <= size; xx++ {
+					sum += intermediate[coord{xx, y}]
+				}
+			} else {
+				sum = sum - intermediate[coord{x - 1, y}] + intermediate[coord{x + size - 1, y}]
+			}
 			if sum > max {
 				max = sum
 				xans = x
@@ -59,15 +74,15 @@ type ans struct {
 
 func part2(serial int) (coord, int) {
 	ch := make(chan ans, 1)
-	for size := 1; size <= 300; size++ {
+	for size := 1; size <= maxSize; size++ {
 		go func(s int) {
-			c, power := maxPower(size, serial)
+			c, power := maxPower(s, serial)
 			ch <- ans{c, s, power}
 		}(size)
 	}
 
 	ansSoFar := ans{size: 0, power: math.MinInt64}
-	for size := 1; size <= 300; size++ {
+	for i := 1; i <= maxSize; i++ {
 		answer := <-ch
 		if answer.power > ansSoFar.power {
 			ansSoFar = answer
