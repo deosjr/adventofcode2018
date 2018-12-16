3483f1c1c2fb188ce0cedae8105018632e1ecde7
Sjoerd Dost <deosjr@gmail.com>
2018-12-16 16:44:59 +0100

    day 15 all testcases pass but bugged (Go)

---
+++ 15/day15.go
@@ -0,0 +1,420 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
+)
+
+type coord struct {
+	x, y int
+}
+
+type tile struct {
+	isWall bool
+	unit   Unit
+}
+
+type Unit interface {
+	HP() int
+	Pos() coord
+	Damage() bool
+	Enemy(Unit) bool
+	MoveTo(coord)
+}
+
+type unit struct {
+	hp  int
+	pos coord
+}
+
+func (u *unit) HP() int {
+	return u.hp
+}
+func (u *unit) Pos() coord {
+	return u.pos
+}
+func (u *unit) Damage() bool {
+	u.hp -= 3
+	return u.hp <= 0
+}
+func (u *unit) MoveTo(c coord) {
+	u.pos = c
+}
+
+type elf struct {
+	*unit
+}
+
+func (*elf) Enemy(u Unit) bool {
+	_, ok := u.(*goblin)
+	return ok
+}
+
+type goblin struct {
+	*unit
+}
+
+func (*goblin) Enemy(u Unit) bool {
+	_, ok := u.(*elf)
+	return ok
+}
+
+type gameState struct {
+	tiles map[coord]tile
+	units []Unit // sorted by pos
+}
+
+func parse(input string) gameState {
+	tiles := map[coord]tile{}
+	units := []Unit{}
+	for y, line := range strings.Split(string(input), "\n") {
+		for x, c := range line {
+			pos := coord{x, y}
+			switch c {
+			case '.':
+				tiles[pos] = tile{}
+			case '#':
+				tiles[pos] = tile{isWall: true}
+			case 'E':
+				e := &elf{&unit{hp: 200, pos: pos}}
+				units = addToSorted(units, e)
+				tiles[pos] = tile{unit: e}
+			case 'G':
+				g := &goblin{&unit{hp: 200, pos: pos}}
+				units = addToSorted(units, g)
+				tiles[pos] = tile{unit: g}
+			}
+		}
+	}
+	return gameState{
+		tiles: tiles,
+		units: units,
+	}
+}
+
+func round(game gameState) ([]Unit, bool) {
+	casualties := map[Unit]struct{}{}
+	for _, u := range game.units {
+		if _, ok := casualties[u]; ok {
+			continue
+		}
+		if game.combatOver(casualties) {
+			return nil, true
+		}
+		t := game.target(u)
+		if t == nil {
+			// move
+			game.move(u)
+
+			// retarget
+			t = game.target(u)
+			if t == nil {
+				continue
+			}
+		}
+		//combat
+		if lethal := t.Damage(); lethal {
+			casualties[t] = struct{}{}
+			game.tiles[t.Pos()] = tile{}
+		}
+	}
+	newUnits := []Unit{}
+	for _, u := range game.units {
+		if _, ok := casualties[u]; ok {
+			continue
+		}
+		newUnits = addToSorted(newUnits, u)
+	}
+	return newUnits, false
+}
+
+// adjacent should only be called for non-walls
+// !ok should therefore never occur
+func (game gameState) adjacentTiles(p coord) []tile {
+	adj := []tile{}
+	neighbours := []coord{{p.x, p.y - 1}, {p.x - 1, p.y}, {p.x + 1, p.y}, {p.x, p.y + 1}}
+	for _, c := range neighbours {
+		t, ok := game.tiles[c]
+		if !ok || t.isWall {
+			continue
+		}
+		adj = append(adj, t)
+	}
+	return adj
+}
+
+func (game gameState) adjacentEmptyCoords(p coord) []coord {
+	coords := []coord{}
+	neighbours := []coord{{p.x, p.y - 1}, {p.x - 1, p.y}, {p.x + 1, p.y}, {p.x, p.y + 1}}
+	for _, c := range neighbours {
+		t, ok := game.tiles[c]
+		if !ok || t.isWall || t.unit != nil {
+			continue
+		}
+		coords = append(coords, c)
+	}
+	return coords
+}
+
+func (game gameState) possibleTargets(u Unit) map[coord]struct{} {
+	targetCoords := map[coord]struct{}{}
+	for _, unit := range game.units {
+		if unit == u {
+			continue
+		}
+		if !u.Enemy(unit) {
+			continue
+		}
+		for _, c := range game.adjacentEmptyCoords(unit.Pos()) {
+			targetCoords[c] = struct{}{}
+		}
+	}
+	return targetCoords
+}
+
+func (game gameState) floodFill(u Unit, targetCoords map[coord]struct{}) (found []coord, floodFillMap map[coord]int) {
+	floodFillMap = map[coord]int{u.Pos(): 0}
+	found = []coord{}
+	i := 0
+	fringe := []coord{u.Pos()}
+	for len(fringe) != 0 {
+		i++
+		newFringe := []coord{}
+
+		for _, f := range fringe {
+			for _, c := range game.adjacentEmptyCoords(f) {
+				if _, ok := floodFillMap[c]; ok {
+					continue
+				}
+				floodFillMap[c] = i
+				newFringe = append(newFringe, c)
+				if _, ok := targetCoords[c]; ok {
+					found = append(found, c)
+				}
+			}
+		}
+
+		if len(found) > 0 {
+			break
+		}
+		fringe = newFringe
+	}
+	return found, floodFillMap
+}
+
+func (game gameState) move(u Unit) {
+	// find possible target locations
+	targetCoords := game.possibleTargets(u)
+	if len(targetCoords) == 0 {
+		return
+	}
+	// expand to all tiles with increasing stepsize
+	found, floodFillMap := game.floodFill(u, targetCoords)
+	if len(found) == 0 {
+		return
+	}
+	// find path to closest target and pick step 1 on the path
+	end := firstInReadingOrder(found)
+	first := game.findFirstStep(end, floodFillMap)
+	// move u to coord first
+	game.step(u, first)
+}
+
+func (game gameState) findFirstStep(end coord, ffm map[coord]int) coord {
+	length := ffm[end]
+	c := end
+	for i := length - 1; i > 0; i-- {
+		found := false
+		for _, adj := range game.adjacentEmptyCoords(c) {
+			if ffm[adj] == i {
+				// if multiple adjacent with same score
+				// pick the one in reading order
+				if found && order2d(c, adj) {
+					continue
+				}
+				found = true
+				c = adj
+			}
+		}
+	}
+	return c
+}
+
+func (game gameState) step(u Unit, c coord) {
+	old := game.tiles[c]
+	old.unit = u
+	game.tiles[c] = old
+	told := game.tiles[u.Pos()]
+	told.unit = nil
+	game.tiles[u.Pos()] = told
+	u.MoveTo(c)
+}
+
+func (game gameState) target(u Unit) Unit {
+	var t Unit = nil
+	for _, tile := range game.adjacentTiles(u.Pos()) {
+		if tile.unit == nil {
+			continue
+		}
+		if !u.Enemy(tile.unit) {
+			continue
+		}
+		if t == nil {
+			t = tile.unit
+			continue
+		}
+		if tile.unit.HP() < t.HP() {
+			t = tile.unit
+			continue
+		}
+		if order2d(tile.unit.Pos(), t.Pos()) {
+			t = tile.unit
+		}
+	}
+	return t
+}
+
+func (game gameState) combatOver(casualties map[Unit]struct{}) bool {
+	var first Unit = nil
+	for _, u := range game.units {
+		if _, ok := casualties[u]; ok {
+			continue
+		}
+		if first == nil {
+			first = u
+			continue
+		}
+		if first.Enemy(u) {
+			return false
+		}
+	}
+	return true
+}
+
+func (game gameState) outcome(rounds int) int {
+	sum := 0
+	for _, u := range game.units {
+		if u.HP() > 0 {
+			sum += u.HP()
+		}
+	}
+	return rounds * sum
+}
+
+// adapted from day 13
+func addToSorted(list []Unit, u Unit) []Unit {
+	if len(list) == 0 {
+		return []Unit{u}
+	}
+	// find smallest index for which list[i].pos comes after u.pos in 2d order
+	index := sort.Search(len(list), func(i int) bool { return order2d(u.Pos(), list[i].Pos()) })
+	// not found: m.id is biggest so insert last
+	if index == len(list) {
+		return append(list, u)
+	}
+	return append(list[:index], append([]Unit{u}, list[index:]...)...)
+}
+
+func removeFromSorted(list []unit, u unit) []unit {
+	toRemove := -1
+	for i, v := range list {
+		if v.pos == u.pos {
+			toRemove = i
+		}
+	}
+	if toRemove == -1 {
+		fmt.Println("LIST DID NOT CONTAIN U")
+		return list
+	}
+	if toRemove == 0 {
+		return list[1:]
+	}
+	if toRemove == len(list)-1 {
+		return list[:len(list)-1]
+	}
+	return append(list[:toRemove], list[toRemove+1:]...)
+}
+
+func firstInReadingOrder(found []coord) coord {
+	t := found[0]
+	// if multiple closest found: pick the closest target in reading order
+	if len(found) > 1 {
+		for _, f := range found[1:] {
+			if order2d(f, t) {
+				t = f
+			}
+		}
+	}
+	return t
+}
+
+// order2d returns true if q comes after p in 2d grid order
+func order2d(p, q coord) bool {
+	if q.y > p.y {
+		return true
+	}
+	if q.y < p.y {
+		return false
+	}
+	if q.x > p.x {
+		return true
+	}
+	return false
+}
+
+func part1(game gameState) int {
+	rounds := 0
+	for {
+		units, noTargetsFound := round(game)
+		if noTargetsFound {
+			return game.outcome(rounds)
+		}
+		game.units = units
+		rounds++
+		// fmt.Println(rounds, ":")
+		// fmt.Println(game.testPrint(32, 32, true))
+	}
+}
+
+func main() {
+	input, err := ioutil.ReadFile("day15.input")
+	if err != nil {
+		panic(err)
+	}
+	game := parse(string(input))
+	// fmt.Println(game.testPrint(32, 32, true))
+	fmt.Printf("Part 1: %d\n", part1(game))
+}
+
+func (game gameState) testPrint(xMax, yMax int, printHealth bool) string {
+	s := ""
+	for y := 0; y < yMax; y++ {
+		units := []string{}
+		for x := 0; x < xMax; x++ {
+			t := game.tiles[coord{x, y}]
+			if t.unit == nil {
+				if t.isWall {
+					s += "#"
+					continue
+				}
+				s += "."
+				continue
+			}
+			if _, ok := t.unit.(*elf); ok {
+				s += "E"
+				units = append(units, fmt.Sprintf("E(%d)", t.unit.HP()))
+				continue
+			}
+			s += "G"
+			units = append(units, fmt.Sprintf("G(%d)", t.unit.HP()))
+		}
+		if printHealth && len(units) > 0 {
+			s += "   "
+			s += strings.Join(units, ", ")
+		}
+		s += "\n"
+	}
+	return s[:len(s)-1]
+}
---
+++ 15/day15_test.go
@@ -0,0 +1,545 @@
+package main
+
+import (
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	for i, tt := range []struct {
+		input string
+		want  int
+	}{
+		{
+			input: `#######
+					#.G...#
+					#...EG#
+					#.#.#G#
+					#..G#E#
+					#.....#
+					#######`,
+			want: 27730,
+		},
+		{
+			input: `#######
+					#G..#E#
+					#E#E.E#
+					#G.##.#
+					#...#E#
+					#...E.#
+					#######`,
+			want: 36334,
+		},
+		{
+			input: `#######
+					#E..EG#
+					#.#G.E#
+					#E.##E#
+					#G..#.#
+					#..E#.#
+					#######`,
+			want: 39514,
+		},
+		{
+			input: `#######
+					#E.G#.#
+					#.#G..#
+					#G.#.G#
+					#G..#.#
+					#...E.#
+					#######`,
+			want: 27755,
+		},
+		{
+			input: `#######
+					#.E...#
+					#.#..G#
+					#.###.#
+					#E#G#G#
+					#...#G#
+					#######`,
+			want: 28944,
+		},
+		{
+			input: `#########
+					#G......#
+					#.E.#...#
+					#..##..G#
+					#...##..#
+					#...#...#
+					#.G...G.#
+					#.....G.#
+					#########`,
+			want: 18740,
+		},
+	} {
+		in := strings.Replace(tt.input, "\t", "", -1)
+		game := parse(in)
+		got := part1(game)
+		if got != tt.want {
+			t.Errorf("%d): got %d want %d", i, got, tt.want)
+		}
+	}
+}
+
+func TestAdjacentTiles(t *testing.T) {
+	e := &elf{&unit{pos: coord{2, 2}}}
+	g := &goblin{&unit{pos: coord{2, 3}}}
+	for i, tt := range []struct {
+		tiles map[coord]tile
+		p     coord
+		want  []tile
+	}{
+		{
+			tiles: map[coord]tile{
+				{2, 2}: tile{unit: e},
+				{1, 2}: tile{},
+				{3, 2}: tile{},
+				{2, 1}: tile{},
+				{2, 3}: tile{},
+			},
+			p:    coord{2, 2},
+			want: []tile{{}, {}, {}, {}},
+		},
+		{
+			tiles: map[coord]tile{
+				{2, 2}: tile{unit: e},
+				{1, 2}: tile{isWall: true},
+				{3, 2}: tile{},
+				{2, 1}: tile{isWall: true},
+				{2, 3}: tile{unit: g},
+			},
+			p:    coord{2, 2},
+			want: []tile{{}, {unit: g}},
+		},
+	} {
+		game := gameState{tiles: tt.tiles}
+		got := game.adjacentTiles(tt.p)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%d): got %v want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestAdjacentEmptyCoords(t *testing.T) {
+	e := &elf{&unit{pos: coord{2, 2}}}
+	g := &goblin{&unit{pos: coord{2, 3}}}
+	for i, tt := range []struct {
+		tiles map[coord]tile
+		p     coord
+		want  []coord
+	}{
+		{
+			tiles: map[coord]tile{
+				{2, 2}: tile{unit: e},
+				{1, 2}: tile{},
+				{3, 2}: tile{},
+				{2, 1}: tile{},
+				{2, 3}: tile{},
+			},
+			p:    coord{2, 2},
+			want: []coord{{2, 1}, {1, 2}, {3, 2}, {2, 3}},
+		},
+		{
+			tiles: map[coord]tile{
+				{2, 2}: tile{unit: e},
+				{1, 2}: tile{isWall: true},
+				{3, 2}: tile{},
+				{2, 1}: tile{isWall: true},
+				{2, 3}: tile{unit: g},
+			},
+			p:    coord{2, 2},
+			want: []coord{{3, 2}},
+		},
+	} {
+		game := gameState{tiles: tt.tiles}
+		got := game.adjacentEmptyCoords(tt.p)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%d): got %v want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestMoves(t *testing.T) {
+	for i, tt := range []struct {
+		input string
+		want  string
+	}{
+		{
+			input: `#########
+					#G..G..G#
+					#.......#
+					#.......#
+					#G..E..G#
+					#.......#
+					#.......#
+					#G..G..G#
+					#########`,
+			want: `#########
+					#.G...G.#
+					#...G...#
+					#...E..G#
+					#.G.....#
+					#.......#
+					#G..G..G#
+					#.......#
+					#########`,
+		},
+		{
+			input: `#########
+					#.G...G.#
+					#...G...#
+					#...E..G#
+					#.G.....#
+					#.......#
+					#G..G..G#
+					#.......#
+					#########`,
+			want: `#########
+					#..G.G..#
+					#...G...#
+					#.G.E.G.#
+					#.......#
+					#G..G..G#
+					#.......#
+					#.......#
+					#########`,
+		},
+		{
+			input: `#########
+					#..G.G..#
+					#...G...#
+					#.G.E.G.#
+					#.......#
+					#G..G..G#
+					#.......#
+					#.......#
+					#########`,
+			want: `#########
+					#.......#
+					#..GGG..#
+					#..GEG..#
+					#G..G...#
+					#......G#
+					#.......#
+					#.......#
+					#########`,
+		},
+		{
+			input: `#########
+					#.......#
+					#..GGG..#
+					#..GEG..#
+					#G..G...#
+					#......G#
+					#.......#
+					#.......#
+					#########`,
+			want: `#########
+					#.......#
+					#..GGG..#
+					#..GEG..#
+					#G..G...#
+					#......G#
+					#.......#
+					#.......#
+					#########`,
+		},
+	} {
+		in := strings.Replace(tt.input, "\t", "", -1)
+		game := parse(in)
+		round(game)
+		got := game.testPrint(9, 9, false)
+		want := strings.Replace(tt.want, "\t", "", -1)
+		if got != want {
+			t.Errorf("%d): \ngot \n%v\nwant \n%v\n", i, got, want)
+		}
+	}
+}
+
+func TestPossibleTargets(t *testing.T) {
+	for i, tt := range []struct {
+		input string
+		unit  int
+		want  []coord
+	}{
+		{
+			input: `#####
+					#.G.#
+					#...#
+					#...#
+					#####`,
+			unit: 0,
+			want: []coord{},
+		},
+		{
+			input: `#####
+					#.G.#
+					#####
+					#.E.#
+					#####`,
+			unit: 0,
+			want: []coord{{1, 3}, {3, 3}},
+		},
+		{
+			input: `#####
+					#.G.#
+					#...#
+					#.E.#
+					#####`,
+			unit: 1,
+			want: []coord{{1, 1}, {3, 1}, {2, 2}},
+		},
+		{
+			input: `#######
+					#.G...#
+					#...EG#
+					#.#.#G#
+					#..G#E#
+					#.....#
+					#######`,
+			unit: 1,
+			want: []coord{{1, 1}, {3, 1}, {2, 2}, {5, 1}, {3, 3}, {2, 4}, {3, 5}},
+		},
+	} {
+		in := strings.Replace(tt.input, "\t", "", -1)
+		game := parse(in)
+		unit := game.units[tt.unit]
+		got := game.possibleTargets(unit)
+		wantMap := map[coord]struct{}{}
+		for _, k := range tt.want {
+			wantMap[k] = struct{}{}
+		}
+		if !reflect.DeepEqual(got, wantMap) {
+			t.Errorf("%d): got %v want %v", i, got, wantMap)
+		}
+	}
+}
+
+func TestFloodFill(t *testing.T) {
+	for i, tt := range []struct {
+		input      string
+		unit       int
+		wantFound  []coord
+		wantString string
+		wantFirst  coord
+	}{
+		{
+			input: `#####
+					#.G.#
+					#####
+					#.E.#
+					#####`,
+			unit:      1,
+			wantFound: []coord{},
+			wantString: `#####
+						#.G.#
+						#####
+						#101#
+						#####`,
+		},
+		{
+			input: `#####
+					#.G.#
+					#...#
+					#.E.#
+					#####`,
+			unit:      0,
+			wantFound: []coord{{2, 2}},
+			wantString: `#####
+						#101#
+						#.1.#
+						#.E.#
+						#####`,
+			wantFirst: coord{2, 2},
+		},
+		{
+			input: `#######
+					#.E...#
+					#.....#
+					#...G.#
+					#######`,
+			unit:      0,
+			wantFound: []coord{{4, 2}, {3, 3}},
+			wantString: `#######
+						#10123#
+						#2123.#
+						#323G.#
+						#######`,
+			wantFirst: coord{3, 1},
+		},
+		{
+			input: `#########
+					#G..G..G#
+					#.......#
+					#.......#
+					#G..E..G#
+					#.......#
+					#.......#
+					#G..G..G#
+					#########`,
+			unit:      4,
+			wantFound: []coord{{4, 2}, {2, 4}, {6, 4}, {4, 6}},
+			wantString: `#########
+						#G..G..G#
+						#...2...#
+						#..212..#
+						#G21012G#
+						#..212..#
+						#...2...#
+						#G..G..G#
+						#########`,
+			wantFirst: coord{4, 3},
+		},
+	} {
+		in := strings.Replace(tt.input, "\t", "", -1)
+		game := parse(in)
+		unit := game.units[tt.unit]
+		gotFound, gotMap := game.floodFill(unit, game.possibleTargets(unit))
+		if !reflect.DeepEqual(gotFound, tt.wantFound) {
+			t.Errorf("%d): got %v want %v", i, gotFound, tt.wantFound)
+		}
+		// parse wantString into floodfillmap
+		wantMap := map[coord]int{}
+		split := strings.Split(tt.wantString, "\n")
+		for y := 0; y < len(split); y++ {
+			s := strings.Replace(split[y], "\t", "", -1)
+			for x := 0; x < len(s); x++ {
+				i, err := strconv.Atoi(string(s[x]))
+				if err != nil {
+					continue
+				}
+				wantMap[coord{x, y}] = i
+			}
+		}
+		if !reflect.DeepEqual(gotMap, wantMap) {
+			t.Errorf("%d): got %v want %v", i, gotMap, wantMap)
+		}
+		if len(tt.wantFound) == 0 {
+			continue
+		}
+		gotEnd := firstInReadingOrder(gotFound)
+		gotFirst := game.findFirstStep(gotEnd, gotMap)
+		if gotFirst != tt.wantFirst {
+			t.Errorf("%d): got %v want %v", i, gotFirst, tt.wantFirst)
+		}
+	}
+}
+
+func TestFirstInReadingOrder(t *testing.T) {
+	for i, tt := range []struct {
+		coords []coord
+		want   coord
+	}{
+		{
+			coords: []coord{{0, 0}},
+			want:   coord{0, 0},
+		},
+		{
+			coords: []coord{{4, 2}, {1, 0}, {2, 3}},
+			want:   coord{1, 0},
+		},
+		{
+			coords: []coord{{4, 2}, {3, 3}},
+			want:   coord{4, 2},
+		},
+	} {
+		got := firstInReadingOrder(tt.coords)
+		if got != tt.want {
+			t.Errorf("%d): got %v want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestRoundWithCombat(t *testing.T) {
+	for i, tt := range []struct {
+		input string
+		setHP map[int]int
+		want  string
+	}{
+		{
+			input: `#######
+					#.G...#
+					#...EG#
+					#.#.#G#
+					#..G#E#
+					#.....#
+					#######`,
+			want: `#######
+					#..G..#   G(200)
+					#...EG#   E(197), G(197)
+					#.#G#G#   G(200), G(197)
+					#...#E#   E(197)
+					#.....#
+					#######`,
+		},
+		{
+			input: `#######
+					#G..#E#
+					#E#E.E#
+					#G.##.#
+					#...#E#
+					#...E.#
+					#######`,
+			want: `#######
+					#G.E#E#   G(197), E(200), E(200)
+					#E#..E#   E(194), E(200)
+					#G.##.#   G(200)
+					#...#E#   E(200)
+					#..E..#   E(200)
+					#######`,
+		},
+		{
+			input: `#######
+					#G.E#E#
+					#E#..E#
+					#G.##.#
+					#...#E#
+					#..E..#
+					#######`,
+			want: `#######
+					#GE.#E#   G(194), E(200), E(200)
+					#E#..E#   E(194), E(200)
+					#G.##.#   G(200)
+					#..E#E#   E(200), E(200)
+					#.....#
+					#######`,
+		},
+		{
+			input: `#######
+					#GE.#E#
+					#E#..E#
+					#G.##.#
+					#..E#E#
+					#.....#
+					#######`,
+			want: `#######
+					#GE.#E#   G(194), E(200), E(200)	
+					#E#..E#   E(191), E(200)
+					#G.##.#   G(200)
+					#.E.#.#   E(200)
+					#....E#   E(200)
+					#######`,
+			setHP: map[int]int{3: 197},
+		},
+	} {
+		in := strings.Replace(tt.input, "\t", "", -1)
+		game := parse(in)
+		for k, v := range tt.setHP {
+			u := game.units[k]
+			if e, ok := u.(*elf); ok {
+				e.hp = v
+				continue
+			}
+			u.(*goblin).hp = v
+		}
+		round(game)
+		got := game.testPrint(7, 7, true)
+		want := strings.Replace(tt.want, "\t", "", -1)
+		if got != want {
+			t.Errorf("%d): \ngot \n%v\nwant \n%v\n", i, got, want)
+		}
+	}
+}
